8d815cc78438ef7da9a77cd603d940d979ccb95c
thespeace <thespeace1@gmail.com>
2024-03-18 14:02:19 +0900

    FEAT: Multi check box

--- src/main/java/thespeace/thymeleaf/itemservice/web/form/FormItemController.java
+++ src/main/java/thespeace/thymeleaf/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,25 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /**
+     * <h2>@ModelAttribute의 특별한 사용법</h2>
+     * 등록 폼, 상세화면, 수정 폼에서 모두 서울, 부산, 제주라는 체크 박스를 반복해서 보여주어야 한다. 이렇게 하려면 각각
+     * 의 컨트롤러에서 {@code model.addAttribute(...)}을 사용해서 체크 박스를 구성하는 데이터를 반복해서 넣어주어야
+     * 한다.
+     * <br>
+     * {@code @ModelAttribute}는 이렇게 컨트롤러에 있는 별도의 메서드에 적용할 수 있다.
+     * 이렇게하면 해당 컨트롤러를 요청할 때 `regions`에서 반환한 값이 자동으로 모델(`model`)에 담기게 된다.
+     * 물론 이렇게 사용하지 않고, 각각의 컨트롤러 메서드에서 모델에 직접 데이터를 담아서 처리해도 된다.
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();//순서보장을 위해 LinkedHashMap 사용.
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
 
     @GetMapping("/intro")
     public String intro() {
@@ -41,13 +62,15 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item()); //`th:object`를 적용하려면 먼저 해당 오브젝트 정보를 넘겨주어야 한다.
-        return "form/addForm";                              //등록 폼이기 때문에 데이터가 비어있는 빈 오브젝트를 만들어서 뷰에 전달하자(빈 객체는 비용이 거의 들지않는다.).
+                                                            //등록 폼이기 때문에 데이터가 비어있는 빈 오브젝트를 만들어서 뷰에 전달하자(빈 객체는 비용이 거의 들지않는다.).
+        return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
