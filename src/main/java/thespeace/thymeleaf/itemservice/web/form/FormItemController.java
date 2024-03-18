6d43502f2cb880f397f9cafea714bd2567093a6f
thespeace <thespeace1@gmail.com>
2024-03-18 14:29:03 +0900

    FEAT: Select box

--- src/main/java/thespeace/thymeleaf/itemservice/web/form/FormItemController.java
+++ src/main/java/thespeace/thymeleaf/itemservice/web/form/FormItemController.java
@@ -1,6 +1,7 @@
 package thespeace.thymeleaf.itemservice.web.form;
 
 import lombok.extern.slf4j.Slf4j;
+import thespeace.thymeleaf.itemservice.domain.item.DeliveryCode;
 import thespeace.thymeleaf.itemservice.domain.item.Item;
 import thespeace.thymeleaf.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import thespeace.thymeleaf.itemservice.domain.item.ItemType;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,6 +53,15 @@ public class FormItemController {
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
         return ItemType.values(); //ItemType.values() : ENUM의 모든 정보를 배열로 반환.
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
     }
 
     @GetMapping("/intro")
